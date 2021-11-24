96789c4c19b8f124fc64e131022b5d691f142461
WasihunT <93289620+WasihunT@users.noreply.github.com>
2021-11-25 00:23:14 +0200

    Update my_mat.c

--- my_mat.c
+++ my_mat.c
@@ -2,15 +2,9 @@
 #include <stdlib.h>
 #include "my_mat.h"
 
-int min(int n, int m);
-int** init();
-void check_path(int **mat);
-void short_path(int **mat);
-
-
 int** init(){ 
 
-int** arr = (int**)malloc(10 * sizeof(int*));
+    int** arr = (int**)malloc(10 * sizeof(int*));
     
     for (int i = 0; i < 10; i++){
         arr[i] = (int*)malloc(10 * sizeof(int));
@@ -23,66 +17,49 @@ int** arr = (int**)malloc(10 * sizeof(int*));
             scanf("%d" ,&arr[i][j]);
         }   
     }
+
+    int k, temp;
+      for(k=0 ; k<10; k++){
+        for(int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+            
+                temp=(arr[i][k]+arr[k][j]);
+
+                if( (arr[i][j] > temp && temp!=0) && (arr[i][k]!=0 && arr[k][j]!=0) ){
+                    arr[i][j]=temp;
+                }
+
+                else if ( (arr[i][j]==0 && (arr[i][k]>0 && arr[k][j]>0)) && (i != j ) ){
+                    arr[i][j]=temp;
+                }
+                
+            }
+        }
+    }
    
    return arr;
   }
 
 
-
-    /**
-     *  func B-check if there is a path between i , j
-     **/
 void check_path(int **mat){
     int i , j;
     scanf("%d", &i);
     scanf("%d", &j);
-    
-    for (int a = 0; a <10; a++){
-     
-        for (int b = 0; b < 10; b++){
 
-           if(a==i && b==j){
-               //check if there is a path
-               if(mat[a][b]!=0){
-                   printf("True\n");
-                   break;
-               }
-               else {
-                   printf("False\n");
-                   break;
-               }
-           }
-        }  
+    if(mat[i][j]!=0){
+        printf("True\n");
+    }
+    else {
+        printf("False\n");
+        
     }
 }
 
-int min(int n, int m){
-   int minnum=n;
-   if(n>m){minnum=m;}
-   return minnum;
-}
 
-    /**
-     *  func C-check if there is a short path between i , j
-     **/
 void short_path(int **mat){
-
-    int i , j, k;
+    int i , j, k,temp;
     scanf("%d", &i);
     scanf("%d", &j);
-    
-    for(k=0 ; k<10; k++){
-        for(int a=0; a<10; a++){
-            for(int b=0; b<10; b++){
-            
-                int temp= mat[a][k]+mat[k][b];
-
-                if( (temp < mat[a][b]) && (temp=!0) && (mat[a][k]!=0 && mat[k][b]!=0) ){
-                    mat[a][b]=mat[a][k]+mat[k][b];
-                }
-            }
-        }
-    }
 
     if(mat[i][j]!=0){
         printf("%d", mat[i][j]);
@@ -91,5 +68,5 @@ void short_path(int **mat){
     else {
      printf("-1\n");
     }
-}
+  }
     
